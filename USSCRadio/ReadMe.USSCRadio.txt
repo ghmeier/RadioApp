15063720a2340c6efc4ad4485f1a80dceee62dd9
ghmeier <meier.garret@gmail.com>
2014-06-05 10:49:20 -0600

    Initial Commit
    Added WebView and all that

---
+++ USSCRadio/ReadMe.USSCRadio.txt
@@ -0,0 +1,4 @@
+Created: 6/5/2014
+By: Garret Meier
+Using Template: MarmaladeQuick Template?
+A Demo of the UCCS Radio application
---
+++ USSCRadio/resources/main.lua
@@ -0,0 +1,25 @@
+local fullScene = director:createScene()
+local names = {"news","schedue","events"}
+fullScene.label = director:createLabel(0,0,"NEWS")
+fullScene.newsView = nui:createWebView({
+	x=0,y=yStart,w=director.displayWidth,h=height,transparentBackground=true,
+	url="http://radio.uccs.edu",
+	} )
+	
+fullScene.scheduleView = nui:createWebView({
+	x=0,y=0,w=director.displayWidth,h=director.isplayHeight,transparentBackground=true,isVisible=false,
+	url="http://radio.uccs.edu/index.php/schedule",
+	} )
+
+
+
+
+function webViewLoadedEvent(event)
+	if (event.type  == "error") then
+		print("Error loading web page" .. event.url)
+	elseif(event.type == "loaded") then
+		print("WebPage Loaded ".. event.url)
+	end
+end
+
+system:addEventListener("webViewLoaded",webViewLoadedEvent)
