13786a6d586ee36178875b7d0d989ee48b12e869
ghmeier <meier.garret@gmail.com>
2014-06-05 12:03:17 -0600

    I can do z indices

--- USSCRadio/resources/main.lua
+++ USSCRadio/resources/main.lua
@@ -6,34 +6,22 @@ fullScene.newsScene.newsView = nui:createWebView({
 	x=0,y=0,w=director.deviceWidth,h=200,transparentBackground=true,
 	url="http://radio.uccs.edu",
 	} )
-fullScene.scheduleScene.scheduleView = nui:createWebView({
-	x=0,y=200,w=director.deviceWidth,h=200,transparentBackground=true,
+fullScene.newsScene.scheduleView = nui:createWebView({
+	x=0,y=100,w=director.deviceWidth,h=200,transparentBackground=true,
 	url="https://www.youtube.com/watch?v=fPevj0vT7-I",
 	} )
-fullScene.newsScene.newsView.isVisible = true
-fullScene.newsScene.zOrder = -1
-fullScene.scheduleScene.zOrder = 5
+
+fullScene.newsScene.newsView.zOrder = -1
+fullScene.newsScene.scheduleView.zOrder = 5
 fullScene.label = director:createLabel({text="NEWS",x=500,y=0,w=200,h=100})
 fullScene.label.color = {255,0,0}
 fullScene.label.zOrder = 2
-fullScene.sprite = director:createSprite(40,40,"textures/HLC.jpg");
+--[[fullScene.sprite = director:createSprite(40,40,"textures/HLC.jpg");
 fullScene.sprite.rotation = -20;
 fullScene.sprite.xScale = .1
 fullScene.sprite.yScale = .1
 fullScene.sprite.color = {255,0,0}
-fullScene.sprite.zORder = 3
---[[fullScene.newsView = nui:createWebView({
-	x=0,y=yStart,w=director.displayWidth,h=height,transparentBackground=true,
-	url="http://radio.uccs.edu",
-	} )
-	
-fullScene.scheduleView = nui:createWebView({
-	x=0,y=0,w=director.displayWidth,h=director.isplayHeight,transparentBackground=true,isVisible=false,
-	url="http://radio.uccs.edu/index.php/schedule",
-	} )
-]]
-
-
+fullScene.sprite.zORder = 3]]
 
 function webViewLoadedEvent(event)
 	if (event.type  == "error") then
