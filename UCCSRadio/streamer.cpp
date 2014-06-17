9a2f6e121901c4ebb82cf824b25ad85479dfe008
Luke Geiken <lukedrum4@gmail.com>
2014-06-17 11:06:13 -0600

    20 px touch limit

--- UCCSRadio/streamer.cpp
+++ UCCSRadio/streamer.cpp
@@ -51,7 +51,7 @@ void Streamer::Update(float deltaTime, float alphaMul)
     // Detect screen tap
     if (!g_pInput->m_Touched && g_pInput->m_PrevTouched && sceneSwitchComplete)
     {
-        if(playButton->HitTest(g_pInput->m_X, g_pInput->m_Y) && g_pInput->m_X == x && g_pInput->m_Y == y) {
+        if(playButton->HitTest(g_pInput->m_X, g_pInput->m_Y) && g_pInput->m_X > x - 20 && g_pInput->m_X < x + 20 && g_pInput->m_Y > y - 20 && g_pInput->m_Y < y + 20) {
             x = 0;
             y = 0;
             g_pInput->Reset();
@@ -62,14 +62,14 @@ void Streamer::Update(float deltaTime, float alphaMul)
 			//Pass a function pointer as 3rd argument to get a callback when audio actually starts playing
 			setVolume(99);
         
-        } else if(stopButton->HitTest(g_pInput->m_X, g_pInput->m_Y) && g_pInput->m_X == x && g_pInput->m_Y == y) {
+        } else if(stopButton->HitTest(g_pInput->m_X, g_pInput->m_Y) && g_pInput->m_X > x - 20 && g_pInput->m_X < x + 20 && g_pInput->m_Y > y - 20 && g_pInput->m_Y < y + 20) {
             g_pInput->Reset();
             playButton->m_X = IwGxGetScreenWidth() / 2.0;
             stopButton->m_X = IwGxGetScreenWidth() * 2.0;
             setVolume(0);
         }
         
-		if (labelLeft->HitTest(g_pInput->m_X, g_pInput->m_Y) && g_pInput->m_X == x && g_pInput->m_Y == y) {
+		if (labelLeft->HitTest(g_pInput->m_X, g_pInput->m_Y) && g_pInput->m_X > x - 20 && g_pInput->m_X < x + 20 && g_pInput->m_Y > y - 20 && g_pInput->m_Y < y + 20) {
             sceneSwitchComplete = false;
             g_pInput->Reset();
             if(currentPage == 0) {
@@ -98,7 +98,7 @@ void Streamer::Update(float deltaTime, float alphaMul)
             }
             
 		}
-		else if (labelRight->HitTest(g_pInput->m_X, g_pInput->m_Y) && g_pInput->m_X == x && g_pInput->m_Y == y) {
+		else if (labelRight->HitTest(g_pInput->m_X, g_pInput->m_Y) && g_pInput->m_X > x - 20 && g_pInput->m_X < x + 20 && g_pInput->m_Y > y - 20 && g_pInput->m_Y < y + 20) {
             sceneSwitchComplete = false;
             g_pInput->Reset();
             if(currentPage == 0) {
